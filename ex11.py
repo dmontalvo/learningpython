ec50b74e7e54beafe04740f39c056c36a1bbd271
Daniel Montalvo <daniel.m@archive.org>
2010-05-26 19:19:48 +0000

    Adding ex5-13

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print "how old are you?",
+age = raw_input()
+print "How many tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
---
+++ ex9.py
@@ -0,0 +1,12 @@
+# Here's some new strange stuff, remember to type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFed\nMar\nApr\nMay\nJun\nJul\nAug"
+print "Here's the days: ", days
+print "Here's the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+"""
